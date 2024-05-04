aa62ca2c24efcfc8a1a5f2dc6e225c04e13b9599
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-03 22:09:26 -0600

    Se cambia el logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
-import logo from '../assets/img/Escudo.svg'
+import logo from '../assets/img/Logo.png'
+import '../pages/auth/authPage.css'
 
 export const Logo = ({ text }) => {
     return (
         <div className='auth-form-logo-container'>
-            <img src={logo} alt="Escudo" />
+            <img src={logo} alt="Logo" />
             <span>{text}</span>
         </div>
     )
