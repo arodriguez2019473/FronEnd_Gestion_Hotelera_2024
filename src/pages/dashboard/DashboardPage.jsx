ccb13639625f3720e6684d1630aeb4fd9a889e6c
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-03 20:31:33 -0600

    Se crean etiquetas para empezar el dise;o

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,5 +1,14 @@
+import './dashboardPage.css';
+
 export const DashboardPage = () => {
     return (
-        <div>DashboardPage</div>
+        <div className="dashboard-container">
+            <form action="settings-form" className=''></form>
+            <h1>Email:</h1>
+            <input type="text" className='input-Llogin' />
+            <h1>Password:</h1>
+            <input type="text" className='input-Llogin' />
+    
+        </div>
     )
 }
