3598bd06e9b16e19b2e290365ccc2896ac58d841
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-19 17:39:21 -0600

    Se corrige ruta

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import  {DashboardPage} from "./pages/dashboard/DashboardPage.jsx";
 import { AuthPage } from "./pages/auth/AuthPage.jsx";
 import {HotelPage} from "./pages/hotel/HotelPage.jsx";
+import RoomPage from "./pages/room/RoomPage.jsx";
 
 const routes = [
     { path: '/auth', element: <AuthPage /> },
    { path: '/hotel', element: <HotelPage /> },
-    { path: '/', element: <DashboardPage /> }
+    { path: '/', element: <DashboardPage /> },
+    { path: '/room/rooms/:hotelId', element: <RoomPage /> }
 ]
 
 export default routes;
