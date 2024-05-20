cd91558b13419116d3a9ee6dc9b90d19da995df0
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-19 19:51:20 -0600

    Se agrega imagen a la tarjeta y se ordena

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './css/HotelCard.css';
 
-const HotelCard = ({hotelId, nameHotel, address, description }) => {
+const HotelCard = ({hotelId, imgUrl, nameHotel, address, description }) => {
 
     const navigate = useNavigate();
 
@@ -14,6 +14,9 @@ const HotelCard = ({hotelId, nameHotel, address, description }) => {
     return (
         <div className="hotel-card">
             <div className="hotel-info">
+                <div className="image-container">
+                    <img src={imgUrl} alt={`${nameHotel}`} className="hotel-image" />
+                </div>
                 <h2 className="hotel-name">{nameHotel}</h2>
                 <p className="hotel-address">{address}</p>
                 <p className="hotel-description">{description}</p>
