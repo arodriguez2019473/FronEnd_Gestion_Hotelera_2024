3e59907361a741af3847e35152ca11b061e944cc
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-21 22:13:52 -0600

    Se crea la rute para calendario

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,15 @@
 import  {DashboardPage} from "./pages/dashboard/DashboardPage.jsx";
 import { AuthPage } from "./pages/auth/AuthPage.jsx";
 import {HotelPage} from "./pages/hotel/HotelPage.jsx";
+import { Calendary } from "./pages/calendary/Calendary.jsx";
 import RoomPage from "./pages/room/RoomPage.jsx";
 
 const routes = [
     { path: '/auth', element: <AuthPage /> },
-   { path: '/hotel', element: <HotelPage /> },
+    { path: '/hotel', element: <HotelPage /> },
     { path: '/', element: <DashboardPage /> },
-    { path: '/room/rooms/:hotelId', element: <RoomPage /> }
+    { path: '/room/rooms/:hotelId', element: <RoomPage /> },
+    { path: 'calendary', element: <Calendary/>}
 ]
 
 export default routes;
