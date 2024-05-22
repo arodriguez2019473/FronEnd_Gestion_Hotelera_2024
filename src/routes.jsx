3697bb888f3bf973640ec4886ead7f5c31e75922
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2024-05-22 01:37:54 -0600

    se crea apartado de graficas

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,13 +3,15 @@ import { AuthPage } from "./pages/auth/AuthPage.jsx";
 import {HotelPage} from "./pages/hotel/HotelPage.jsx";
 import RoomPage from "./pages/room/RoomPage.jsx";
 import We from "./pages/we/We.jsx";
+import { Grafica } from "./pages/graphic/Grafica.jsx";
 
 const routes = [
     { path: '/auth', element: <AuthPage /> },
    { path: '/hotel', element: <HotelPage /> },
     { path: '/', element: <DashboardPage /> },
     { path: '/room/rooms/:hotelId', element: <RoomPage /> },
-    { path: '/we', element: <We/> }
+    { path: '/we', element: <We/> },
+    { path: '/graphic', element: <Grafica/>}
 ]
 
 export default routes;
